e46f7d5e6fde46855c1b74ff1558692c0fd47362
Shreya Sahu <sahush2004@gmail.com>
2024-06-20 00:01:41 +0530

    feat: Add solution for setting matrix zeros

---
+++ Day1_Array/SetMatrixZero.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void SetMatrixZero(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[] row = new int[n]; // row array
+        int[] col = new int[m]; // col array
+
+        // Traverse the matrix:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    // mark ith index of row with 1:
+                    row[i] = 1;
+
+                    // mark jth index of col with 1:
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Finally, mark all (i, j) as 0
+        // if row[i] or col[j] is marked with 1.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+
+        sol.SetMatrixZero(matrix);
+
+        System.out.println("The Final matrix is: ");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
